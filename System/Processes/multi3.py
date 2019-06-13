e8dd42c543f56629f623fd288feb86ca8be71742
chukreev-gl <chukreev.g@gmail.com>
2019-06-13 10:24:08 +0300

    added stream directory 1

--- System/Processes/multi3.py
+++ System/Processes/multi3.py
@@ -14,10 +14,6 @@ count = 0
 def showdata(label, val, arr):
     """
     вывод значений данных в проессе
-    :param label:
-    :param val:
-    :param arr:
-    :return:
     """
     msg = '%-12s: pid:%4s, global:%s, value:%s, array:%s'
     print(msg % (label, os.getpid(), count, val.value, list(arr)))
@@ -26,9 +22,6 @@ def showdata(label, val, arr):
 def updater(val, arr):
     """
     Обмен данными через разделяему память
-    :param val:
-    :param arr:
-    :return:
     """
     global count
     count += 1
@@ -39,7 +32,7 @@ def updater(val, arr):
 if __name__ == '__main__':
     scalar = Value('i', 0)
     vector = Array('d', procs)
-    # вывод начальногозначения в родительскомм процессе
+    # вывод начального значения в родительскомм процессе
     showdata('parent start', scalar, vector)
 
     # порождение дочернего процесса и передача данных в разделяемую память
@@ -47,23 +40,37 @@ if __name__ == '__main__':
     p.start();
     p.join()
 
-    # изменение значения в родителе и передача через разделяему память,
+    # 1. изменение значения в родителе и передача через разделяему память,
     # ожидание завершения каждого потомка
     # все потомки видят изменения, выполенные в родительском процессе и
     # переданные в виде аргументов (но не в глобальной памяти)
 
-    print('\nloop2(updates in parent, parallel children)...')
+    print('\nloop1(updates in parent, parallel children)...')
     ps = []
     for i in range(procs):
         count += 1
         scalar.value += 1
         vector[i] += 1
         p = Process(target=showdata, args=(('process %s' % i), scalar, vector))
+        p.start(); p.join()
+
+    # 2. то же самоей, но потомки запускаются параллельно
+    # все видят результат последней итерации, потому что они хранятся
+    # в совместно используемых объектах
+    print('\nloop2 (updated in parent, parallel children)...')
+    ps=[]
+    for i in range(procs):
+        count+=1
+        scalar.value+=1
+        vector[i]+=1
+        p=Process(target=showdata, args=(('process %s' % i), scalar, vector))
+
         p.start()
         ps.append(p)
     for p in ps: p.join()
 
-    # объекты в разделяемой памяти изменяются потомками,
+
+    # 3. объекты в разделяемой памяти изменяются потомками,
     # ожидание завершения каждого из них
     print('\nloop3 (updates in serial children)...')
     for i in range(procs):
@@ -72,7 +79,7 @@ if __name__ == '__main__':
         p.join()
         showdata('parent temp', scalar, vector)
 
-    # то же самое, но потомки запускаются параллельно
+    # 4. то же самое, но потомки запускаются параллельно
     ps = []
     print('\nloop4 (updates in parallel children)...')
     for i in range(procs):
@@ -83,5 +90,4 @@ if __name__ == '__main__':
 
     # глобальная переменная count=6 доступна только родителю
     # выведет последние результаты # scalar=12: +6 в родителе, +6 в 6 потомках
-    showdata('parent end', scalar, vector)  # array[i]=8:
-    # +2 в родителе, +6 в 6 потомках
+    showdata('parent end', scalar, vector)  # array[i]=8: +2 в родителе, +6 в 6 потомках
