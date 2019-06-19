8269beb19f8e3acbc56b8a982b7ffa06a7557336
chukreev-gl <chukreev.g@gmail.com>
2019-06-19 17:35:02 +0300

    added 16/06/19

---
+++ Gui/Tour/entry2.py
@@ -0,0 +1,40 @@
+"""
+непосредственное использование виджетов Entry и размещение их по рядам с метками
+фиксированной ширины: такой способ компоновки, а также использование менеджера
+grid обеспечивают наилучшее представление для форм
+"""
+from tkinter import *
+from quitter import Quitter
+
+fields = 'Name', 'Job', 'Pay'
+
+
+def fetch(entries):
+    for entry in entries:
+        print('Input => "%s"' % entry.get())
+
+
+def makeform(root, fields):
+    entries = []
+    for field in fields:
+        row = Frame(root)
+        lab = Label(row, width=5, text=field)
+        ent = Entry(row)
+        row.pack(side=TOP, fill=X)
+        lab.pack(side=LEFT)
+        ent.pack(side=RIGHT, expand=YES, fill=X)
+        entries.append(ent)
+    return entries
+
+
+if __name__ == '__main__':
+    root = Tk()
+
+    # root.config(font=('times', 16,'italic'))
+
+    ents = makeform(root, fields)
+    root.bind('<Return>', (lambda event: fetch(ents)))
+    Button(root, text='Fetch',
+           command=(lambda: fetch(ents))).pack(side=LEFT)
+    Quitter(root).pack(side=RIGHT)
+    root.mainloop()
