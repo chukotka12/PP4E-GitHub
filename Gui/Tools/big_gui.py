9c6cfc1042513d4e33bfbecea4dd97d35cb46276
chukreev-gl <chukreev.g@gmail.com>
2019-08-21 17:45:04 +0300

    added big_qui

---
+++ Gui/Tools/big_gui.py
@@ -0,0 +1,90 @@
+"""
+реализация графического интерфейса - объединяет GuiMaker, GuiMixin и данный
+класс
+"""
+import sys, os
+from tkinter import *
+from Gui.Tools.guimixin import *
+from Gui.Tools.guimaker import *
+
+
+class Hello(GuiMixin, GuiMakerWindowMenu):  # или GuiMakerFrameMenu
+    def start(self):
+        self.hellos = 0
+        self.master.title('GuiMaker Demo')
+        self.master.iconname('GuiMaker')
+
+        def spawnme(): self.spawn('big_gui.py')  # отложенный вызов вместо lambda
+
+        self.menuBar = [
+            ('File', 0,
+             [('New...', 0, spawnme),
+              ('Open...', 0, self.fileOpen),
+              ('Quit', 0, self.quit)]),
+            ('Edit', 0,
+             [('Cut', -1, self.notdone),  # без клавиши|обработчика
+              ('Paste', -1, self.notdone),  # можно и lambda:0
+              'separator',
+              ('Stuff', -1,
+               [('Clone', -1, self.clone),
+                ('More', -1, self.more)]),
+              ('Delete', -1, lambda: 0),
+              [5]]  # отключить 'delete'
+             ),
+            ('Play', 0,
+             [('Hello', 0, self.greeting),
+              ('Popup...', 0, self.dialog),
+              ('Demos', 0,
+               [('Toplevels', 0,
+                 lambda: self.spawn(r'..\Tour\toplevel2.py')),
+                ('Frames', 0,
+                 lambda: self.spawn(r'..\Tour\demoAll-frm-ridge.py')),
+                ('Images', 0,
+                 lambda: self.spawn(r'..\Tour\buttonpics.py')),
+                ('Alarm', 0,
+                 lambda: self.spawn(r'..\Tour\alarm.py', wait=False)),
+                ('Other...', -1, self.pickDemo)])])
+        ]
+        self.toolBar = [
+            ('Quit', self.quit, dict(side=RIGHT)),
+            ('Hello', self.greeting, dict(side=LEFT)),
+            ('Popup', self.dialog, dict(side=LEFT, expand=YES))
+        ]
+
+    def makeWidgets(self):
+        middle = Label(self, text='Hello maker world',
+                       widht=40, height=10,
+                       relief=SUNKEN, cursor='pensil', bg='white')
+        middle.pack(expand=YES, fill=BOTH)
+
+    def greeting(self):
+        self.hellos += 1
+        if self.hellos % 3:
+            print('hi')
+        else:
+            self.infobox('Three', 'HELLO!')
+
+    def dialog(self):
+        button = self.question('OOPS!', 'You typed "rm*"... continue?',
+                               'questhead', ('yes', 'no'))
+        [lambda: None, self.quit][button]()
+
+    def fileOpen(self):
+        pick = self.selectOpenFile(file='big_gui.py')
+        if pick:
+            self.browser(pick)
+
+    def more(self):
+        new = Toplevel()
+        Label(new, text='A new non-modal window').pack()
+        Button(new, text='Quit', command=self.quit).pack(side=LEFT)
+        Button(new, text='More', command=self.more).pack(side=RIGHT)
+
+    def pickDemo(self):
+        pick = self.selectOpenFile(dir='..')
+        if pick:
+            self.spawn(pick)
+
+
+if __name__ == '__main__':
+    Hello().mainloop()
